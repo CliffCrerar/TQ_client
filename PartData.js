fb4236cd04f4190d71d56236bbf241390c657826
Cliff Crerar <cliff.crerar@gmail.com>
2017-07-14 21:59:46 +0200

    Created JS parts object

---
+++ PartData.js
@@ -0,0 +1,28 @@
+var KTM_Parts = {
+    A08255:
+    {
+        partType: 'Luggage Rack',
+        partDesc: 'Made from lightweight suspect alluminium, '+
+                  'used to tie down appropriate luggage with straps included with the luggage wrack.'+
+                  'Also includes a securing kit.',
+        models: ['KTM 1050','KTM 1090','KTM 1290'],
+        imgLink: 'http://tinyurl.com/ybw5lg64',
+        colors: 'Black',
+        grnt: '1 year',
+        instTime: 30,
+        price: 1120,
+    },
+    A08201:
+    {
+        partType: 'Bash Plate',
+        partDesc: 'Made from ? , protects the engine casing from loose projectiles and road hazards, must have for offroading.',
+        models: ['KTM 950','KTM 990'],
+        imgLink: 'http://tinyurl.com/yage98cq',
+        colors: 'Black',
+        grnt: '1 Year',
+        instTime: 45,
+        price: 2395,
+
+    }
+
+}
