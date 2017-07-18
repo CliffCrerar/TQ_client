c2887517375cda2aabed477deb50f441bcb935bf
Clive01 <clive.crerar@gmail.com>
2017-07-18 10:03:23 +0200

    Ad PARTS

---
+++ KTM_Parts.js
@@ -0,0 +1,216 @@
+  /**************************/
+ /*   KTM BIKE PARTS DATA  */
+/**************************/ 
+
+var KTM_Parts = [
+    
+    //Part A08255
+    {  
+        partNum: 'A08255',
+        partName: 'Luggage Rack',
+        partDesc: 'Made from lightweight suspect alluminium, '+
+                  'used to tie down appropriate luggage with straps included with the luggage rack.'+
+                  'Also includes a securing kit.',
+        models: ['KTM 1050','KTM 1090','KTM 1290'],
+        imgLink: 'http://tinyurl.com/ybw5lg64',
+        colors: 'Black',
+        grnt: '1 year',
+        instTime: 30,
+        price: 1120,
+    },
+    
+    //Part A08201
+    {
+        partNum: 'A08201',
+        partName: 'Bash Plate',
+        partDesc: 'Made from ? , protects the engine casing from loose projectiles and road hazards, must have for offroading.',
+        models: ['KTM 950','KTM 990'],
+        imgLink: 'http://tinyurl.com/yage98cq',
+        colors: 'Black',
+        grnt: '1 Year',
+        instTime: 45,
+        price: 2395,
+    },
+    //Part A08271
+    {  
+        partNum: 'A08271',
+        partName: 'Head Light Mesh Guard',
+        partDesc: 'Manufactured from stainless steel, made with quick release connectors for easy cleaning.',
+        models: ['KTM 1050','KTM 1090','KTM 1190','KTM 1290(pre 2017)'],
+        imgLink: 'http://tinyurl.com/y7l54cgu',
+        colors: 'Stainless Steel',
+        grnt: '1 Year',
+        instTime: 30 ,
+        price: 1425 ,
+    },
+    //Part A11150
+    {  
+        partNum: 'A11150',
+        partName: 'Top Box Bracket',
+        partDesc: 'Made from aluminium and is used to mount top box above rear mud guard. Can be used in off road conditions. ',
+        models: ['KTM 1190'],
+        imgLink: 'http://tinyurl.com/ycj2l37v',
+        colors: 'Aluminium',
+        grnt: '1 Year',
+        instTime: 30 ,
+        price: 755 ,
+    },
+    //Part A08250
+    {
+        partNum: 'A08250',
+        partName: 'Standard Luggage Rack',
+        partDesc: 'Made from lightweight suspect alluminium, '+
+                  'used to tie down appropriate luggage with straps included with the luggage rack.'+
+                  'Also includes a securing kit.',
+        models: ['KTM 1190'],
+        imgLink: 'http://tinyurl.com/y8jy6jgq',
+        colors: 'Black',
+        grnt: '1 Year',
+        instTime: 30,
+        price: 1120,
+    },
+    //Part A08260
+    {  
+        partNum: 'A08260',
+        partName: 'Big Foot',
+        partDesc: 'Manufactured from aluminium and is sold with a rubber insert. Prevents side stand from sinking into soft surfaces and cracks in paving.',
+        models: ['KTM 1190','KTM 1290','KTM 1050'],
+        imgLink: 'http://tinyurl.com/yaj42fdx',
+        colors: 'Silver',
+        grnt: '1 Year',
+        instTime: 30 ,
+        price: 365,
+    },
+    //Part 'A08400'
+    {  
+        partNum: 'A08400',
+        partName: 'Top Box',
+        partDesc: 'Manufactured from 2mm natural aluminium has a capacity of 41 volumetric litres. Requires top box plate A11155 to be mounted to bike ',
+        models: ['KTM 1290 Adventure'],
+        imgLink: 'http://tinyurl.com/y8t9ulwu',
+        colors: 'Natural Aliminuim',
+        grnt: '1 Year',
+        instTime: 10,
+        price: 3930,
+    },
+    //Part A11155
+    {  
+        partNum: 'A11155',
+        partName: 'Bracket Top Box',
+        partDesc: 'Bracket to secure top box to top of mudguard',
+        models: ['KTM 1290'],
+        imgLink: 'http://tinyurl.com/y86j87c3',
+        colors: 'Stainless steel',
+        grnt: '1 Year',
+        instTime: 30,
+        price: 755,
+    },
+    //Part A08258
+    {  
+        partNum: 'A8258',
+        partName: 'Luggage Rack',
+        partDesc: 'Made from lightweight suspect alluminium, used to secure appropiate luggage with straps included in luggage rack, also includes a securing kit',
+        models: ['KTM 1050','KTM 1090','KTM 1190','KTM 1290'],
+        imgLink: 'http://tinyurl.com/yd87phcd',
+        colors: 'Black',
+        grnt: '1 year',
+        instTime: 30 ,
+        price: 1120,
+    }, 
+    //Part A08273
+    { 
+        partNum: 'A08273',
+        partName: 'Head Light Mess Guard',
+        partDesc: 'Manufactured from stainless steel, made with quick connectors for easy cleaning',
+        models: ['KTM 1290','KTM 1050','KTM 1090','KTM1190(Pre 2017)'],
+        imgLink: 'http://tinyurl.com/yaq7kfmr',
+        colors: 'Stainless Steel',
+        grnt: '1 Year',
+        instTime: 15,
+        price: 1425,
+    },
+    //Part A11143
+    {  
+        partNum: 'A11143',
+        partName: 'Luggage Rack',
+        partDesc: 'Manufactured from stainless steel and 19.05mm stainless tube, can carry Enduro Pannier or soft luggage.',
+        models: ['KTM 690'],
+        imgLink: 'http://tinyurl.com/y89qcx35',
+        colors: 'Stainless Steel',
+        grnt: ' 1 year',
+        instTime: 60 ,
+        price: 2780,
+    },
+    //Part A08200
+    {  
+        partNum: 'A08200',
+        partName: 'Bash Plate',
+        partDesc: 'Made off aliminuim. Protects the engine casing from lose protectiles and road hazards must have for off roading',
+        models: ['KTM960','KTM 990',''],
+        imgLink: 'http://tinyurl.com/ybdwkblj',
+        colors: 'Naturel Anodized',
+        grnt: '1 year',
+        instTime: 60,
+        price: 2395,
+    },
+    //Part A08070
+    {  
+        partNum: 'A08070',
+        partName: 'Luggage rack Adventure',
+        partDesc: 'Manufactured from aluminium, suitable for carry light weight soft luggage secured with straps supplied and fitting kit',
+        models: ['KTM 950','KTM 990',''],
+        imgLink: 'http://tinyurl.com/yb3y6ahf',
+        colors: 'Silver',
+        grnt: '1 Year',
+        instTime: 30,
+        price: 1120,
+    },
+    //Part A08060
+    {  
+        partNum: 'A08060',
+        partName: 'Side Stand Extender Adventure ',
+        partDesc: 'Manufactured from aluminium, helps keep bike straight up',
+        models: ['KTM 950','KTM 990'],
+        imgLink: 'http://tinyurl.com/ycq8voj7',
+        colors: 'Black',
+        grnt: '1 Year',
+        instTime: 20,
+        price: 340,
+    },
+    //Part A11140
+    {  
+        partNum: 'A11140',
+        partName: 'Top Bracket Adventure',
+        partDesc: 'Manufactured from aliminuim, secures the top even off roading',
+        models: ['KTM 950','KTM 990',''],
+        imgLink: 'http://tinyurl.com/y7w5o2yo',
+        colors: 'Silver',
+        grnt: '!1 Year',
+        instTime: 30,
+        price: 735,
+    },
+   //Part A08010
+    {  
+        partNum: 'A08010',
+        partName: 'Front brake Disc Guard',
+        partDesc: 'Manufactured from aliminuim, protects the front brake disc from loose projectiles and road hazards',
+        models: ['KTM EXE','KTM XC','KTM XC-W','KTM XCF','KTM XCF-W' ],
+        imgLink: 'http://tinyurl.com/y9bb6bb4',
+        colors: 'Aliminuim',
+        grnt: '1 Year',
+        instTime: 50,
+        price: 1890,
+    },
+     //Part A08020
+    {  
+        partNum: 'A08020',
+        partName: 'Rear Brake Disc Guard',
+        partDesc: 'Manufactured from aliminuim, protects the rear brake disc from loose projectiles and road hazards',
+        models: ['KTM EXC','KTM XC KTM XC-W','KTM XCF','KTM XCF-W'],
+        imgLink: '',
+        colors: '',
+        grnt: '',
+        instTime: ,
+        price: ,
+    }
+]
