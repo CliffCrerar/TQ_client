e12eac81c74bc98f16e47516183e62fc5e35d5b3
Clive01 <clive.crerar@gmail.com>
2017-07-25 09:42:35 +0200

    Add Part

--- LRDef_parts.js
+++ LRDef_parts.js
@@ -6,10 +6,10 @@ var LRDef_parts = [
     //Part A22000
     {  
         partNum: 'A22000',
-        partName: 'Bull BAr with Winch Bracket',
+        partName: 'Bull Bar with Winch Bracket',
         partDesc: 'Manufactured from steel with capacity to accommodate a winch and the ability protected the complete front end of the vehicle in the bush as well in an accident',
         models: ['90','110','130'],
-        imgLink: '',
+        imgLink: 'http://tinyurl.com/yaz57c38',
         colors: 'Powder Coat Black',
         grnt: '1 Year',
         instTime: 120,
@@ -38,41 +38,5 @@ var LRDef_parts = [
         grnt: '1 Year',
         instTime: 45,
         price: 925,
-    },
-    //Part A22016
-    {  
-        partNum: 'A22016',
-        partName: 'Bonnet Plate',
-        partDesc: 'Manufactured from aluminium reinforces the bonnet to accommodate more weight',
-        models: ['90','110','130'],
-        imgLink: 'http://tinyurl.com/y75m9v5g',
-        colors: 'Black',
-        grnt: '1 Year',
-        instTime: 30,
-        price: 2495,
-    },
-    //Part A22016
-    {  
-        partNum: 'A22016',
-        partName: 'Fuel Filer Protection Plate',
-        partDesc: 'Manufactured from aluminium protects paint work around the filler cap',
-        models: ['110','130'],
-        imgLink: 'http://tinyurl.com/y79r9ob8',
-        colors: 'Silver',
-        grnt: '1 Year',
-        instTime: 30,
-        price: 280,
-    },
-    //Part A22017
-    {  
-        partNum: 'A22017',
-        partName: 'Fuel Filer Protection Plate',
-        partDesc: 'Manufactured from aluminium protects paint work around the filler cap',
-        models: ['90'],
-        imgLink: 'http://tinyurl.com/ybzl4klx',
-        colors: 'Silver',
-        grnt: '1 Year',
-        instTime: 30,
-        price: 280,
     }
-]
+    
