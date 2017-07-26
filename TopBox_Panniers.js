eb3d1f159472070820847432636c8fbc0527851c
Clive01 <clive.crerar@gmail.com>
2017-07-26 11:55:05 +0200

    Add Parts

--- TopBox_Panniers.js
+++ TopBox_Panniers.js
@@ -3,17 +3,56 @@
 /****************************/
 
 var TopBoxPannier = [
-    //Part A08255     
+    //Part A08400     
     {
-        partNum: '',
-        partName: '',
-        partDesc: '',
-        models: ['','',''],
-        imgLink: '',
-        colors: '',
-        grnt: '',
-        instTime: 0,
-        price: 0,
-        cat: ''
+        partNum: 'A08400',
+        partName: 'Top Box',
+        partDesc: 'Manufactured from 2 mm aluminium and has 41 litre volumetric capacity and requires a A11155 Top Plate to mount onto the bike',
+        models: ['KTM 1290 Adventure'],
+        imgLink: 'http://tinyurl.com/y8t9ulwu',
+        colors: 'Silver',
+        grnt: '1 Year',
+        instTime: 30,
+        price: 3930,
+        cat: 'TB'
     },
+    //Part A11110
+    {  
+        partNum: 'A11110',
+        partName: 'Top Box',
+        partDesc: 'Manufactured from 2 mm aluminium walls with stainless and has 41 litre volumetric capacity .',
+        models: ['BMW R1200GS/R1200GS Adventure'],
+        imgLink: 'http://tinyurl.com/y7fts7tc',
+        colors: 'Silver',
+        grnt: '1 Year',
+        instTime: 30,
+        price: 3930,
+        cat: 'TB'
+    },
+    //Part A11111
+    {  
+        partNum: 'A11111',
+        partName: 'Top Box',
+        partDesc: 'Manufactured from 2 mm aluminium walls with stainless and has 41 litre volumetric capacity',
+        models: ['BMW R1200GS/R1200GS Adventure'],
+        imgLink: 'http://tinyurl.com/y8v4rhqe',
+        colors: 'New Silver',
+        grnt: '1 Year',
+        instTime: 30,
+        price: 4040,
+        cat: 'TB', 
+    },
+       //Part A11112
+    {  
+        partNum: 'A11112',
+        partName: 'Top Box',
+        partDesc: 'Manufactured from 2 mm aluminium walls with stainless and has 41 litre volumetric capacity',
+        models: ['BMW R1200GS/R1200GS Adventure'],
+        imgLink: 'http://tinyurl.com/y9ltemvy',
+        colors: 'New Silver',
+        grnt: '1 Year',
+        instTime: 30,
+        price: 4150,
+        cat: 'TB', 
+    }   
 ]
