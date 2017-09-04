d36144c8c49ad4e3bc4a30d3bb4060757a1811c2
Clive01 <clive.crerar@gmail.com>
2017-09-04 13:27:11 +0200

    Deleted Part

--- BMW_Parts.js
+++ BMW_Parts.js
@@ -887,18 +887,5 @@ var BMW_parts = [
         price: 940,
         cat: 'WFP', 
     },  
-    //Part A09630
-    {  
-        partNum: 'A09630',
-        partName: 'Wide Foot Pegs Pillion',
-        partDesc: 'Makes riding more comfortable for passenger and less chance of your foot slipping off the pegs when wet',
-        models: ['BMW R1200GS/R1200GS Adventure LC K50 K51'],
-        imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A09630/547ed67a6d9d8_A09630-PIC0.JPG&length=1200',
-        colors: 'Silver',
-        grnt: '1 Year',
-        instTime: 30,
-        price: 940,
-        cat: 'WFP', 
-    },
-        
+           
 ]
