652d0e746506d79e14e9d99743c74b053ec4843d
Cliff Crerar <cliff.crerar@gmail.com>
2017-11-17 21:05:38 +0200

    COmm

---
+++ HND_parts.js
@@ -0,0 +1,33 @@
+  /****************************/
+ /*   HONDA BIKE PARTS DATA  */
+/****************************/ 
+
+HND_parts = [
+    //Part A08255
+    {  
+        partNum: 'A08255TESTPAR',
+        partName: 'HONDA',
+        partDesc: 'Honda test part for part testing',
+        models: ['Kawazaku honda','Some more','Some hondas'],
+        imgLink: 'http://tinyurl.com/ybw5lg64',
+        colors: 'Black',
+        grnt: '1 year',
+        instTime: 30,
+        price: 10000,
+        cat: 'LR'
+    },
+    {  
+        partNum: 'A08255',
+        partName: 'Luggage Rack',
+        partDesc: 'Made from lightweight suspect alluminium, '+
+                  'used to tie down appropriate luggage with straps included with the luggage rack.'+
+                  'Also includes a securing kit.',
+        models: ['KTM 1050','KTM 1090','KTM 1290'],
+        imgLink: 'http://tinyurl.com/ybw5lg64',
+        colors: 'Black',
+        grnt: '1 year',
+        instTime: 30,
+        price: 1120,
+        cat: 'LR'
+    }
+] 
