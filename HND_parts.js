d0596bbcc870bbd0cb05cfb89fb0d475ad10a8e8
Clive01 <clive.crerar@gmail.com>
2017-12-01 11:47:55 +0200

    moved commer

--- HND_parts.js
+++ HND_parts.js
@@ -2,7 +2,7 @@
  /*   HONDA BIKE PARTS DATA  */
 /****************************/ 
 
-HND_parts = [
+var HND_parts = [
     //Part A040360
     {  
         partNum: 'A040360',
@@ -12,20 +12,7 @@ HND_parts = [
         imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A040360/5964be2875356_MA-A040360-01.JPG&length=600',
         colors: 'Natural Aluminium',
         grnt: '1 Year',
-        instTime: 30 ,
+        instTime: 30,
         price: 1900,
         cat: 'BP'
     },      
-    //Part A040361
-    {  
-        partNum: 'A040361',
-        partName: 'Bash Plate Black',
-        partDesc: 'Made from Aluminium and Stainless Steel Powder Coated Black',
-        models: ['Honda CRF1000L Africa Twin Africa'],
-        imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A040361/5964be2894366_MA-A040360-04.JPG&length=600',
-        colors: 'Black',
-        grnt: '1 Year',
-        instTime: 30,
-        price: 2030,
-        cat: 'BP', 
-    }   
