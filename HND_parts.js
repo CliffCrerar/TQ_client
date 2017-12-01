fa6d14721d9dbc55e6629e11523c329141e2e5f3
Clive01 <clive.crerar@gmail.com>
2017-12-01 11:35:41 +0200

    added parts

--- HND_parts.js
+++ HND_parts.js
@@ -6,8 +6,8 @@ HND_parts = [
     //Part A040360
     {  
         partNum: 'A040360',
-        partName: 'BASH PLATE',
-        partDesc: 'BASH PLATE MADE FROM ALUMINIUM AND STAINLESS STEEL ANODIZED NATURAL ALUMINIUM ',
+        partName: 'Bash Plate Alumiunim',
+        partDesc: 'Made from Aluminium and Stainless Steel Anodized Natural Aluminium ',
         models: ['Honda CRF1000L Africa Twin'],
         imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A040360/5964be2875356_MA-A040360-01.JPG&length=600',
         colors: 'Natural Aluminium',
@@ -15,6 +15,17 @@ HND_parts = [
         instTime: 30 ,
         price: 1900,
         cat: 'BP'
-    },
-
-] 
+    },      
+         //Part A040361
+    {  
+        partNum: 'A040361',
+        partName: 'Bash Plate Black',
+        partDesc: 'Made from Aluminium and Stainless Steel Powder Coated Black',
+        models: ['Honda CRF1000L Africa Twin Africa'],
+        imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A040361/5964be2894366_MA-A040360-04.JPG&length=600',
+        colors: 'Black',
+        grnt: '1 Year',
+        instTime: 30,
+        price: 2030,
+        cat: 'BP', 
+    }   
