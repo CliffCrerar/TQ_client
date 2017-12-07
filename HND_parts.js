a6ae4658375af66854004751a992c91135324cba
Clive01 <clive.crerar@gmail.com>
2017-12-07 15:27:04 +0200

    Add part

--- HND_parts.js
+++ HND_parts.js
@@ -2,17 +2,19 @@
  /*   HONDA BIKE PARTS DATA  */
 /****************************/ 
 
-var HND_parts = [
-    //Part A040360
-    {          
-        partNum: 'A040360',
-        partName: 'Bash Plate Alumiunim',
-        partDesc: 'Made from Aluminium and Stainless Steel Anodized Natural Aluminium',
-        models: ['Honda CRF1000L Africa Twin'],
-        imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A040360/5964be2875356_MA-A040360-01.JPG&length=600',
-        colors: 'Natural Aluminium',
+HND_parts = [
+     //Part *A040310*
+     {  
+        partNum: 'A040310',
+        partName: 'BRAKE RESERVOIR PROTECTOR',
+        partDesc: 'REAR BRAKE RESERVOIR PROTECTOR',
+        models: ['HONDA CRF1000L AFRICA TWIN '],
+        imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A040310/582aa7729544b_A040310-(4).jpg&length=600',
+        colors: 'Silver',
         grnt: '1 Year',
-        instTime: 30,
-        price: 1900,
-        cat: 'BP'
-    },      
+        instTime: '30',
+        price: '340',
+        cat: 'BRP'
+    },
+
+]
