3ff7ed7b1c86afdb4d6c3c487f3d083141fe328a
Clive01 <clive.crerar@gmail.com>
2017-12-07 15:32:36 +0200

    removed commers

--- HND_parts.js
+++ HND_parts.js
@@ -12,8 +12,8 @@ HND_parts = [
         imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A040310/582aa7729544b_A040310-(4).jpg&length=600',
         colors: 'Silver',
         grnt: '1 Year',
-        instTime: '30',
-        price: '340',
+        instTime: 30,
+        price: 340,
         cat: 'BRP'
     },
 
