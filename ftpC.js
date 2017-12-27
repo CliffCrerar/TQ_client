b4a897e6e73cf9efdc0073d6d2f95f9aa67b649c
CliffordCrerar <cliff.crerar@gmail.com>
2017-12-27 12:47:23 +0200

    this

---
+++ ftpC.js
@@ -0,0 +1,11 @@
+var Client = require('ftp');
+
+var c = new Client();
+c.on('ready', function() {
+    c.list(function(err, list) {
+        if (err) throw err;
+        console.dir(list);
+        c.end();
+    });
+});
+c.connect();
--- index.js
+++ index.js
@@ -1,34 +1,16 @@
 console.log('Starting web server');
 
-var hostIP = newFunction();
-var port = '8080';
-
 var http = require('http');
 var fs = require('fs');
 
 var html = fs.readFileSync('index.html');
 
+var hostIP = '127.0.0.1'
+var port = '4000';
+
 http.createServer(function(req, res) {
     res.writeHeader(200, { 'Content-Type': 'text/html' });
     res.end(html);
-
 }).listen(port, hostIP);
 
 console.log('Node server running on socket ' + hostIP + ':' + port);
-
-console.log('Hosting Bootstrap Document Library')
-
-var bootstrapDocs = fs.readFileSync('ftp/bootstrap/index.html')
-
-var bsCocsPort = 8085
-
-http.createServer(function(req, res) {
-    res.writeHeader(200, { 'Content-Type': 'text/html' });
-    res.end(a);
-}).listen(bootstrapDocs, hostIP);;
-
-console.log('Hosting Bootstrap docs on socket ' + hostIP + ':' + port);
-
-function newFunction() {
-    return '127.0.0.1';
-}
