80b10ca82f2778fea3b88db9f54039c36fe01738
Cliff <cliff.crerar@gmail.com>
2017-12-27 04:11:39 +0200

    Bootstrap jekyl

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+console.log('Starting web server');
+
+var hostIP = '127.0.0.1';
+var port = '8080';
+
 var http = require('http');
 var fs = require('fs');
 
 var html = fs.readFileSync('index.html');
 
-console.log('File Server Running');
-
 http.createServer(function(req, res) {
     res.writeHeader(200, { 'Content-Type': 'text/html' });
     res.end(html);
-}).listen(8080, '127.0.0.1');
+}).listen(port, hostIP);
 
-console.log('Node server running on 8080');
+console.log('Node server running on socket ' + hostIP + ':' + port);
