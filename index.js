1fb1df569268b08faec8ff0392b085b809f1daac
Cliff <cliff.crerar@gmail.com>
2017-12-27 02:37:45 +0200

    this

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var http = require('http');
 var fs = require('fs');
 
 var html = fs.readFileSync('index.html');
+
+console.log('File Server Running');
+
 http.createServer(function(req, res) {
-        res.writeHeader(200, { 'Content-Type': 'text/html' });
-        res.end(html);
-    })
-    .listen(8080);
+    res.writeHeader(200, { 'Content-Type': 'text/html' });
+    res.end(html);
+}).listen(8080, '127.0.0.1');
 
 console.log('Node server running on 8080');
