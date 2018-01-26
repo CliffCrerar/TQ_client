33c5c1c57940b86899e1a4d5893753d4eaefd0b7
Clive01 <clive.crerar@gmail.com>
2018-01-26 15:09:49 +0200

    Add Parts

--- HND_parts.js
+++ HND_parts.js
@@ -28,6 +28,32 @@
         instTime: 30,
         price: 330,
         cat: 'SSE'
-    }
-
+    },
+      //Part A040355
+      {  
+        partNum: 'A040355',
+        partName: 'Radiator Guards',
+        partDesc: 'Protects the Raditor from stone and loose projectiles on the road (Made from Stainless Steel) (sold as a set)',
+        models: ['Honda CRF1000L'],
+        imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A040355/582aa772ae642_Honda%20AT%20Radiator%20Protectors%20Pic%20(3).jpg&length=600',
+        colors: 'Silver',
+        grnt: '1 Year',
+        instTime: 30,
+        price: 610,
+        cat: 'RP'
+    },   
+   //Part  A040360
+   {  
+    partNum: ' A040360',
+    partName: 'Bash Plate',
+    partDesc: 'Protects the under side of the engine from loose projectiles and is made of Aluminium, stainless steel and anodized silver',
+    models: ['Honda CRF1000L'],
+    imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A040360/59f9f682ec6a1.jpg&length=600',
+              'http://www.motorradical.co.za/thumb.php?path=img/products/A040360/5964be2875356_MA-A040360-01.JPG&length=350', 
+    colors: 'Silver',
+    grnt: '1 Year',
+    instTime: 30,
+    price: 1910,
+    cat: 'BP'
+   }, 
     ]
