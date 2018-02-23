b6899604c9c15cf751eb64e9cfabe99c69e6b6f9
Clifford Crerar <cliff.crerar@gmail.com>
2018-02-23 23:11:46 +0200

    gitignore

--- webServer/app.js
+++ webServer/app.js
@@ -2,10 +2,10 @@ var http = require('http');
 var partsData = require('./combineParts.js');
 
 
-//var IP = '172.16.0.152';
+var IP = '172.16.0.152';
 //var IP = '172.20.10.3';
 //var IP = '172.16.0.103';
-var IP = '172.17.20.16';
+//var IP = '172.17.20.16';
 var PORT1 = 8000;
 //var PORT2 = 8001;
 
