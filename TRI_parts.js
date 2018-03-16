ad524031a319cd5833432833a1bc4fa53f0efc80
Clive01 <clive.crerar@gmail.com>
2018-03-16 06:07:49 +0200

    Added parts

--- TRI_parts.js
+++ TRI_parts.js
@@ -3,18 +3,18 @@
   /**********************************/
   /*
     TRI_parts = [
-        //Part **
+        //Part *A05000*
         {
-            partNum: '',
-            partName: '',
-            partDesc: '',
-            models: [' ', ' ', ' '],
-            imgLink: '',
-            colors: '',
-            grnt: '',
-            instTime: '',
-            price: '',
-            cat: ''
+            partNum: 'A05000',
+            partName: '800 and 800XC',
+            partDesc: 'Make riding more comfortable and less chance of your foot slipping off the pegs when wet',
+            models: ['800', '800XC',],
+            imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A05000/560070c45b4ac_Triumph%20Tiger%20Wide%20Foot%20pegs%20(2).JPG&length=600',
+            colors: 'Silver',
+            grnt: '0',
+            instTime: '.5',
+            price: '865',
+            cat: 'WFP'
         },
 
     ]
