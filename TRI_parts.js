4c402195bd052ffd86a5dddb9f944cf8089aabfe
Clive01 <clive.crerar@gmail.com>
2018-03-16 15:25:14 +0200

    removed part

--- TRI_parts.js
+++ TRI_parts.js
@@ -2,21 +2,5 @@
   /*   AFRICA TWIN BIKE PARTS DATA  */
   /**********************************/
   
-    TRI_parts = [
-        //Part A05000
-        {
-            partNum: 'A05000',
-            partName: 'Wide Foot Pegs',
-            partDesc: 'Make riding more comfortable and less chance of your foot slipping off the pegs when wet',
-            models: ['800','800XC'],
-            imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A05000/560070c45b4ac_Triumph%20Tiger%20Wide%20Foot%20pegs%20(2).JPG&length=600',
-            colors: 'Silver',
-            grnt: '0',
-            instTime: .30,
-            price: 865,
-            cat: 'WFP'
-        },
-
+  var  TRI_parts= [
         
-            
-        },
