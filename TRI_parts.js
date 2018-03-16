257c1ec8414c1ed7a6b92989f5329e64e7abb7a6
Clive01 <clive.crerar@gmail.com>
2018-03-16 15:13:19 +0200

    commers

--- TRI_parts.js
+++ TRI_parts.js
@@ -6,14 +6,14 @@
         //Part A05000
         {
             partNum: 'A05000',
-            partName: '800 and 800XC',
+            partName: 'Wide Foot Pegs',
             partDesc: 'Make riding more comfortable and less chance of your foot slipping off the pegs when wet',
-            models: ['800', '800XC',],
+            models: ['800','800XC'],
             imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A05000/560070c45b4ac_Triumph%20Tiger%20Wide%20Foot%20pegs%20(2).JPG&length=600',
             colors: 'Silver',
             grnt: '0',
-            instTime: '.30',
-            price: '865',
+            instTime: .30,
+            price: 865,
             cat: 'WFP'
         },
 
@@ -22,11 +22,12 @@
             partNum: 'A05005',
             partName: 'Side Stand Extender',
             partDesc: 'Manufactured from aluminium with a rubber foot pad helps with non slip',
-            models: ['800','800XC',],
+            models: ['800','800XC'],
             imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A05005/546fa0ba28af3_A05005-PIC04.JPG&length=600',
             colors: 'Black and Silver',
             grnt: '0',
-            instTime: '30',
+            instTime: 30,
+            price: 305,
             cat: 'SSE' 
             
         },
