fba99db40b8b4bd93ff21856ebcd6763c9e7f22b
Clive01 <clive.crerar@gmail.com>
2018-03-16 14:35:21 +0200

    Added PArst

--- TRI_parts.js
+++ TRI_parts.js
@@ -12,10 +12,23 @@
             imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A05000/560070c45b4ac_Triumph%20Tiger%20Wide%20Foot%20pegs%20(2).JPG&length=600',
             colors: 'Silver',
             grnt: '0',
-            instTime: '.5',
+            instTime: '.30',
             price: '865',
             cat: 'WFP'
         },
 
-    ]
+        //Part A05005
+        {  
+            partNum: 'A05005',
+            partName: 'Side Stand Extender',
+            partDesc: 'Manufactured from aluminium with a rubber foot pad helps with non slip',
+            models: ['800','800XC'],
+            imgLink: 'http://www.motorradical.co.za/thumb.php?path=img/products/A05005/546fa0ba28af3_A05005-PIC04.JPG&length=600',
+            colors: 'Black and Silver',
+            grnt: '',
+            instTime:'30' ,
+            price: '305',
+            cat: 'SSE', 
+            
+    },
     
