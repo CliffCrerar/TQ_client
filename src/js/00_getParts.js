13bbf39c497ab3b7eeebd412b0e16ef255905fb1
Clifford Crerar <cliff.crerar@gmail.com>
2018-03-26 21:23:21 +0200

    updating web server for multi type requests

--- src/js/00_getParts.js
+++ src/js/00_getParts.js
@@ -1,45 +1,26 @@
-var partsData;
-console.log('00.1_Parts request started');
+console.log("00.1_Parts request started");
 
 //const URL = 'http://34.242.179.249:8010';
-var URL = 'http://172.16.0.104:8010';
+var URL = "http://172.16.0.104:8010/";
 //var URL = 'http://196.32.249.85:8000';
 //var URL = 'http://172.20.10.3:8000';
 
-
-
 $.ajax({
-    url: URL,
-    type: 'GET',
-    success: (response) => {
-        window.partsData = JSON.parse(response);
-        console.log('00.2_Parts data request successful');
-        console.log(URL);
-        for (var key in window.partsData) {
-            //console.log(window.partsData[key]);
-            //console.log('SP_Z_1', window.partsData[key].salePriceZAR);
-            //console.log('SP_U_1', window.partsData[key].salePriceUSD);
-            var spz1 = Math.ceil(window.partsData[key].salePriceZAR / 10) * 10;
-            var spu1 = Math.ceil(window.partsData[key].salePriceUSD / 10) * 10;
-            //console.log('SP_Z_2', spz1);
-            //console.log('SP_U_2', spu1);
-            var spz2 = Number(spz1 - 1).toLocaleString('en-za');
-            var spu2 = Number(spu1 - 1).toLocaleString('en-US');
-            //console.log('SP_Z_3', 'R ' + spz2);
-            //console.log('SP_U_3', '$ ' + spu2);
-            window.partsData[key].salePriceZAR = 'R ' + spz2;
-            window.partsData[key].salePriceUSD = '$ ' + spu2;
-
-        }
-    },
-    error: (error, xhr, more) => {
-        console.log(URL);
-        window.partsData = '!!!!Parts data not loaded';
-        console.log('An error has occured during parts data');
-        console.log(error);
-        console.log(xhr);
-        console.log(more);
-
-    }
+  url: URL + "getParts",
+  type: "GET",
+  success: response => {
+    console.log(response);
+    window.partsData = response;
+    console.log("00.2_Parts data request successful");
+    console.log(URL);
+  },
+  error: (error, xhr, more) => {
+    console.log(URL);
+    window.partsData = "!!!!Parts data not loaded";
+    console.log("An error has occured during parts data");
+    console.log(error);
+    console.log(xhr);
+    console.log(more);
+  }
 });
-console.log('00_getParts.loaded');
+console.log("00_getParts.loaded");
